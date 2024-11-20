27f4958cd8d54cdab098b36e571b82bbee549374
vn-phihoang <vn_phihoang@tentac.co.jp>
2024-11-20 15:52:56 +0700

    update

--- TENTAC_HRM/Properties/Resources.Designer.cs
+++ TENTAC_HRM/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace TENTAC_HRM.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -533,6 +533,16 @@ namespace TENTAC_HRM.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ClearContent {
+            get {
+                object obj = ResourceManager.GetObject("ClearContent", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap clock {
             get {
                 object obj = ResourceManager.GetObject("clock", resourceCulture);
@@ -906,6 +916,16 @@ namespace TENTAC_HRM.Properties {
         internal static System.Drawing.Bitmap hierarchy_45x45 {
             get {
                 object obj = ResourceManager.GetObject("hierarchy_45x45", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap importExcel {
+            get {
+                object obj = ResourceManager.GetObject("importExcel", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
