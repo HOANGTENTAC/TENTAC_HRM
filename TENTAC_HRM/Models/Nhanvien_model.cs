be78c167eb1b1188c1b9bcd171fc53d118a5eff5
vn-phihoang <vn_phihoang@tentac.co.jp>
2024-11-27 13:48:01 +0700

    Update model

--- TENTAC_HRM/Models/Nhanvien_model.cs
+++ TENTAC_HRM/Models/Nhanvien_model.cs
@@ -9,7 +9,7 @@ namespace TENTAC_HRM.Model
         public int Hon_nhan_value { get; set; }
         public string Ton_giao_value { get; set; }
         public string Dan_toc_value { get; set; }
-        public int Quoc_tich_value { get; set; }
+        public string Quoc_tich_value { get; set; }
         public int Hoc_van_value { get; set; }
         public float? Chieu_cao_value { get; set; }
         public int Nhan_tien_mat_value { get; set; }
@@ -33,7 +33,7 @@ namespace TENTAC_HRM.Model
         public string Noi_cap_hc_value { get; set; }
         public string Sdt_value { get; set; }
         public string Email_value { get; set; }
-        public string MaCongTy_value { get;set; }
+        public string MaCongTy_value { get; set; }
         public string MaKhuVuc_value { get; set; }
         public string MaPhongBan_value { get; set; }
         public string MaChucVu_value { get; set; }
@@ -41,7 +41,7 @@ namespace TENTAC_HRM.Model
         public string Nhom_mau_value { get; set; }
         public string Luu_y_sk_value { get; set; }
         public int Khuyet_tat_value { get; set; }
-        public string Can_nang_value { get; set; }
+        public float? Can_nang_value { get; set; }
         public string Ghi_chu_value { get; set; }
         public string Ma_so_thue_value { get; set; }
         public DateTime? Ngay_sinh_value { get; set; }
@@ -55,11 +55,10 @@ namespace TENTAC_HRM.Model
         public DateTime? Ngay_dk_thue_value { get; set; }
         public DateTime? Ngay_cap_work_permit_value { get; set; }
         public DateTime? Ngay_het_han_work_permit_value { get; set; }
-
         public string Ma_Cham_Cong { get; set; }
-
         public string Ten_Cham_Cong { get; set; }
-
         public string Ma_The { get; set; }
+        public string LoaiNhanVien { get; set; }
+        public string ReportTo { get; set; }
     }
 }
